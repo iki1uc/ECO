2e55a40ab3d468b8e91f68fc4ef6a0f46b2f9846
iki1uc <entertrainer@web.de>
2026-08-05 10:24:26 +0200

    Update import.js

--- import.js
+++ import.js
@@ -1,4 +1,4 @@
-// import.js – AUTO‑PATH für MXU/ECO
+// import.js – AUTO-PATH für alle Module
 
 export async function ECO_IMPORT() {
 
@@ -11,45 +11,7 @@ export async function ECO_IMPORT() {
     const RESPO = await import(ROOT + "respo.js");
     const AXINXA = await import(ROOT + "AXINXA.js");
     const TXT = await import(ROOT + "txt/raw.txt");
+    const NC = await import(ROOT + "NCCheck.js");
 
-    return { MXU, RESPO, AXINXA, TXT };
+    return { MXU, RESPO, AXINXA, TXT, NC };
 }
-
-
-https://iki1uc.github.io/MXU/INDEX.html
-
-https://iki1uc.github.io/MXU/ID.html
-
-https://iki1uc.github.io/MXU/ATOM.html
-
-https://iki1uc.github.io/MXU/BOERSE.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/MARKT.html
-
-https://iki1uc.github.io/MXU/SELF.html
-
-https://iki1uc.github.io/MXU/toolomatter.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/self.html
-
-https://iki1uc.github.io/MXU/DOM/DOM.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/MARKT/markt.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/BOERSE/boerse.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/EVO/EVO.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/EOS/EOS.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/funktion/funktion.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/respo/respo.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/tool48/pyramide.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/UI/UI.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/UPD/upd.html (iki1uc.github.io in Bing)
-
-https://iki1uc.github.io/MXU/UPG/upg.html (iki1uc.github.io in Bing)
