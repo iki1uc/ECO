a0d5459392f5690380d04108ab30deb563ba06f4
iki1uc <entertrainer@web.de>
2026-08-05 09:50:17 +0200

    Create import.js

---
+++ import.js
@@ -0,0 +1,37 @@
+https://iki1uc.github.io/MXU/INDEX.html
+
+https://iki1uc.github.io/MXU/ID.html
+
+https://iki1uc.github.io/MXU/ATOM.html
+
+https://iki1uc.github.io/MXU/BOERSE.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/MARKT.html
+
+https://iki1uc.github.io/MXU/SELF.html
+
+https://iki1uc.github.io/MXU/toolomatter.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/self.html
+
+https://iki1uc.github.io/MXU/DOM/DOM.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/MARKT/markt.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/BOERSE/boerse.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/EVO/EVO.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/EOS/EOS.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/funktion/funktion.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/respo/respo.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/tool48/pyramide.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/UI/UI.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/UPD/upd.html (iki1uc.github.io in Bing)
+
+https://iki1uc.github.io/MXU/UPG/upg.html (iki1uc.github.io in Bing)
